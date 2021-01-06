67815573d9fd9911125b4391f275b6cbf3be964b
Hanna Littlefield <hannamlittlefield@gmail.com>
2021-01-06 18:35:22 -0500

    initial commit with page css

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <header><h1 className="Title">Markdown Previewer</h1></header>
+    <textarea id="editor"></textarea>
+      <footer className="footer">&copy; 2020 by Hanna Littlefield</footer>
+    </div>
+  );
+}
+
+export default App;
