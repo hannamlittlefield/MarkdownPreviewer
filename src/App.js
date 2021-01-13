9bc009a126a94afc921a3377378b774f71901bf8
Hanna Littlefield <hannamlittlefield@gmail.com>
2021-01-13 10:30:53 -0500

    working on marked function and state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import './App.css';
+import React, {useState} from 'react'
+import marked from 'marked'
 
-function App() {
+const defaultText = '#Markdown Previewer\n\n #A freeCodeCamp Project '
+
+export default function Markdown() {
+  const [text, setText] =useState(defaultText)
+
+function handleChange(event){
+  setText(event.target.text)
+}
+
+function getRawMarkup() { __html: marked(text)};
+
   return (
     <div className="App">
     <header><h1 className="Title">Markdown Previewer</h1></header>
-    <textarea id="editor"></textarea>
+    <textarea 
+    defaultValue={text}
+    id="editor"
+    onChange = {handleChange}
+    />
+    <div id="preview"
+    dangerouslySetInnerHTML={getRawMarkup()}
+    />
       <footer className="footer">&copy; 2020 by Hanna Littlefield</footer>
     </div>
   );
 }
 
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Markdown from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Markdown />
   </React.StrictMode>,
   document.getElementById('root')
 );
